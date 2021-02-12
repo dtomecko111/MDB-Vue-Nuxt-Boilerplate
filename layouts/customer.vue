d8e7242180ad6d917440c290265be0bf5f9d700e
David Tomecko <dtomecko@clearviewenergy.com>
2021-02-12 07:12:22 -0600

    customer

---
+++ layouts/customer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    <SideBarCP />
+    <div id="content-wrapper" class="d-flex flex-column">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      
+      bodyAttrs: {
+        class: "page-top",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
